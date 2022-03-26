6bf26fae8d0a4d59b09273332593d63bffb0e66a
jmalabed <jmalabed27@gmail.com>
2022-03-26 12:23:15 -0700

    added donate buttons

--- src/components/PayPal.jsx
+++ src/components/PayPal.jsx
@@ -13,31 +13,12 @@ const PayPal = (props) => {
       "AVNWl_Aeq_fOYJCIJ52k571ffit4cXHRPvNl2YifNKFS63Zu_gaoHNDq2xu18EVTVMKWux_xlXmh3hGf",
     currency: "USD",
     intent: "capture",
-    "data-client-token": "abc123xyz==",
   };
   return (
     <div>
       <PayPalScriptProvider options={initialOptions}>
         <PayPalButtons style={style} />
       </PayPalScriptProvider>
-      <form action="https://www.paypal.com/donate" method="post" target="_top">
-        <input type="hidden" name="hosted_button_id" value="7VCSU9UDZGATJ" />
-        <input
-          type="image"
-          src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif"
-          border="0"
-          name="submit"
-          title="PayPal - The safer, easier way to pay online!"
-          alt="Donate with PayPal button"
-        />
-        <img
-          alt=""
-          border="0"
-          src="https://www.paypal.com/en_US/i/scr/pixel.gif"
-          width="1"
-          height="1"
-        />
-      </form>
     </div>
   );
 };
